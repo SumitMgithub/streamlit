f078b0fbd34203448d9a17bde80c75f3caf51320
Sumit Malviya <143081763+SumitMgithub@users.noreply.github.com>
2024-04-30 13:40:24 +0530

    Update app_cb.py

--- app_cb.py
+++ app_cb.py
@@ -75,7 +75,7 @@ def main():
             # st.write(docs)
 
             llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff" )
+            chain = load_qa_chain(llm=llm, chain_type="stuff", return_source_documents=True )
             response = chain.run(input_documents=docs, question=query)
             st.write(response)
 
